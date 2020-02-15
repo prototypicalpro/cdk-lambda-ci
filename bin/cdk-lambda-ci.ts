f09e8ddb460dcc9c86492993d425cc14ebfba999
Noah Laptop <noah@koontzs.com>
2020-02-15 14:08:03 -0800

    initial commit

---
+++ bin/cdk-lambda-ci.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkLambdaCiStack } from '../lib/cdk-lambda-ci-stack';
+
+const app = new cdk.App();
+new CdkLambdaCiStack(app, 'CdkLambdaCiStack');
